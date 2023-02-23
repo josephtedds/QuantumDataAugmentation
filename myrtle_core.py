340a2e0d7e758a5e4cde86df8248526a11ca8026
josephtedds <joseph@tedds.co.uk>
2023-02-23 08:32:20 +0000

    feat: myrtle ai code

---
+++ myrtle_core.py
@@ -0,0 +1,227 @@
+import copy
+from collections import namedtuple, defaultdict
+import time
+import numpy as np
+import pandas as pd
+from functools import singledispatch
+
+#####################
+# utils
+#####################
+
+class Timer():
+    def __init__(self, synch=None):
+        self.synch = synch or (lambda: None)
+        self.synch()
+        self.times = [time.perf_counter()]
+        self.total_time = 0.0
+
+    def __call__(self, include_in_total=True):
+        self.synch()
+        self.times.append(time.perf_counter())
+        delta_t = self.times[-1] - self.times[-2]
+        if include_in_total:
+            self.total_time += delta_t
+        return delta_t
+    
+localtime = lambda: time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+
+default_table_formats = {float: '{:{w}.4f}', str: '{:>{w}s}', 'default': '{:{w}}', 'title': '{:>{w}s}'}
+
+def table_formatter(val, is_title=False, col_width=12, formats=None):
+    formats = formats or default_table_formats
+    type_ = lambda val: float if isinstance(val, (float, np.float)) else type(val)
+    return (formats['title'] if is_title else formats.get(type_(val), formats['default'])).format(val, w=col_width)
+
+def every(n, col): 
+    return lambda data: data[col] % n == 0
+
+class Table():
+    def __init__(self, keys=None, report=(lambda data: True), formatter=table_formatter):
+        self.keys, self.report, self.formatter = keys, report, formatter
+        self.log = []
+        
+    def append(self, data):
+        self.log.append(data)
+        data = {' '.join(p): v for p,v in path_iter(data)}
+        self.keys = self.keys or data.keys()
+        if len(self.log) is 1:
+            print(*(self.formatter(k, True) for k in self.keys))
+        if self.report(data):
+            print(*(self.formatter(data[k]) for k in self.keys))
+            
+    def df(self):
+        return pd.DataFrame([{'_'.join(p): v for p,v in path_iter(row)} for row in self.log])     
+
+
+#####################
+## data preprocessing
+#####################
+def preprocess(dataset, transforms):
+    dataset = copy.copy(dataset) #shallow copy
+    for transform in transforms:
+        dataset['data'] = transform(dataset['data'])
+    return dataset
+
+@singledispatch
+def normalise(x, mean, std):
+    return (x - mean) / std
+
+@normalise.register(np.ndarray) 
+def _(x, mean, std): 
+    #faster inplace for numpy arrays
+    x = np.array(x, np.float32)
+    x -= mean
+    x *= 1.0/std
+    return x
+
+unnormalise = lambda x, mean, std: x*std + mean
+
+@singledispatch
+def pad(x, border):
+    raise NotImplementedError
+
+@pad.register(np.ndarray)
+def _(x, border): 
+    return np.pad(x, [(0, 0), (border, border), (border, border), (0, 0)], mode='reflect')
+
+@singledispatch
+def transpose(x, source, target):
+    raise NotImplementedError
+
+@transpose.register(np.ndarray)
+def _(x, source, target):
+    return x.transpose([source.index(d) for d in target]) 
+
+#####################
+## data augmentation
+#####################
+
+class Crop(namedtuple('Crop', ('h', 'w'))):
+    def __call__(self, x, x0, y0):
+        return x[..., y0:y0+self.h, x0:x0+self.w]
+
+    def options(self, shape):
+        *_, H, W = shape
+        return [{'x0': x0, 'y0': y0} for x0 in range(W+1-self.w) for y0 in range(H+1-self.h)]
+    
+    def output_shape(self, shape):
+        *_, H, W = shape
+        return (*_, self.h, self.w)
+
+@singledispatch
+def flip_lr(x):
+    raise NotImplementedError
+
+@flip_lr.register(np.ndarray)
+def _(x): 
+    return x[..., ::-1].copy()
+
+class FlipLR(namedtuple('FlipLR', ())):
+    def __call__(self, x, choice):
+        return flip_lr(x) if choice else x 
+        
+    def options(self, shape):
+        return [{'choice': b} for b in [True, False]]
+
+class Cutout(namedtuple('Cutout', ('h', 'w'))):
+    def __call__(self, x, x0, y0):
+        x[..., y0:y0+self.h, x0:x0+self.w] = 0.0
+        return x
+
+    def options(self, shape):
+        *_, H, W = shape
+        return [{'x0': x0, 'y0': y0} for x0 in range(W+1-self.w) for y0 in range(H+1-self.h)]    
+    
+
+class Transform():
+    def __init__(self, dataset, transforms):
+        self.dataset, self.transforms = dataset, transforms
+        self.choices = None
+        
+    def __len__(self):
+        return len(self.dataset)
+           
+    def __getitem__(self, index):
+        data, labels = self.dataset[index]
+        data = data.copy()
+        for choices, f in zip(self.choices, self.transforms):
+            data = f(data, **choices[index])
+        return data, labels
+    
+    def set_random_choices(self):
+        self.choices = []
+        x_shape = self.dataset[0][0].shape
+        N = len(self)
+        for t in self.transforms:
+            self.choices.append(np.random.choice(t.options(x_shape), N))
+            x_shape = t.output_shape(x_shape) if hasattr(t, 'output_shape') else x_shape
+
+
+#####################
+## dict utils
+#####################
+
+union = lambda *dicts: {k: v for d in dicts for (k, v) in d.items()}
+
+def path_iter(nested_dict, pfx=()):
+    for name, val in nested_dict.items():
+        if isinstance(val, dict): yield from path_iter(val, (*pfx, name))
+        else: yield ((*pfx, name), val)  
+
+def map_nested(func, nested_dict):
+    return {k: map_nested(func, v) if isinstance(v, dict) else func(v) for k,v in nested_dict.items()}
+
+def group_by_key(items):
+    res = defaultdict(list)
+    for k, v in items: 
+        res[k].append(v) 
+    return res
+
+#####################
+## graph building
+#####################
+sep = '/'
+
+def split(path):
+    i = path.rfind(sep) + 1
+    return path[:i].rstrip(sep), path[i:]
+
+def normpath(path):
+    #simplified os.path.normpath
+    parts = []
+    for p in path.split(sep):
+        if p == '..': parts.pop()
+        elif p.startswith(sep): parts = [p]
+        else: parts.append(p)
+    return sep.join(parts)
+
+has_inputs = lambda node: type(node) is tuple
+
+def pipeline(net):
+    return [(sep.join(path), (node if has_inputs(node) else (node, [-1]))) for (path, node) in path_iter(net)]
+
+def build_graph(net):
+    flattened = pipeline(net)
+    resolve_input = lambda rel_path, path, idx: normpath(sep.join((path, '..', rel_path))) if isinstance(rel_path, str) else flattened[idx+rel_path][0]
+    return {path: (node[0], [resolve_input(rel_path, path, idx) for rel_path in node[1]]) for idx, (path, node) in enumerate(flattened)}    
+
+#####################
+## training utils
+#####################
+
+@singledispatch
+def cat(*xs):
+    raise NotImplementedError
+    
+@singledispatch
+def to_numpy(x):
+    raise NotImplementedError
+
+class PiecewiseLinear(namedtuple('PiecewiseLinear', ('knots', 'vals'))):
+    def __call__(self, t):
+        return np.interp([t], self.knots, self.vals)[0]
+ 
+class Const(namedtuple('Const', ['val'])):
+    def __call__(self, x):
+        return self.val
